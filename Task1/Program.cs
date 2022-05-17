077d2a12fc35f3ca334f66d13ecd45405b187e74
DENIS BLINOV <blinovda1@mail.ru>
2022-05-17 21:33:47 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 465->5
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+
+string s_n_1 = Console.ReadLine();
+int n_1 = int.Parse(s_n_1);
+
+int TheSecDigit(int num)
+{
+    return (num / 10 % 10);
+}
+
+Console.Write("Вторая цифра: ");
+Console.WriteLine(TheSecDigit(n_1));
