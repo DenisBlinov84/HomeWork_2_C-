c11434a060bb59eb64731e1c1bb7f2f59457511f
DENIS BLINOV <blinovda1@mail.ru>
2022-05-18 07:20:01 +0300

    add task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,13 @@
 // которая выводит случайное трёхзначное число 
 // и удаляет вторую цифру этого числа.
 // 456->46
+
+void DelSecNum()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine($"Random number: {num}");
+    Console.WriteLine($"Result: {num/100}{num%10}");
+}
+
+DelSecNum();
+
