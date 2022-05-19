54cf7717b7bcfe9f15dde32a05a709a189fb7ce6
DENIS BLINOV <blinovda1@mail.ru>
2022-05-19 19:27:43 +0300

    add task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,23 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 
+Console.Clear();
+Console.Write("Введите число: ");
+
+string s_n_1 = Console.ReadLine();
+int n_1 = int.Parse(s_n_1);
+
+int TheSecDigit(int num)
+{
+    while(num>999) num/=10;
+    return(num%10);
+}
+if (n_1 > 99)
+{
+    Console.Write("Третья цифра: ");
+    Console.WriteLine(TheSecDigit(n_1));
+}
+else
+{
+    Console.Write("Третьей цифры нет ");
+}
