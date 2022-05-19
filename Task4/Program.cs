ff22be48df21064ea9995b73aa4dbfabb7156acf
DENIS BLINOV <blinovda1@mail.ru>
2022-05-19 20:55:26 +0300

    add task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+string s_n_1 = Console.ReadLine();
+int n_1 = int.Parse(s_n_1);
+
+void Weekend(int num)
+{
+    string text = "нет";
+    if (num == 7 || num == 6)
+    {
+        text = "да";
+    }
+    Console.WriteLine($"{num} -> {text}");
+    
+}
+Weekend(n_1);
+
